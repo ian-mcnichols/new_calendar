0f017917b08aa36bc5333f008d714524033e0d5c
Ian McNichols <ian.mcnichols@gmail.com>
2023-06-07 13:40:26 -0700

    Adding day numbers

--- calendar_creation.py
+++ calendar_creation.py
@@ -20,15 +20,28 @@ class Calendar():
             can.line(100, 350, 700, 350)
             can.line(100, 275, 700, 275)
             can.line(100, 200, 700, 200)
-            can.line(100, 125, 700, 125)
+            can.line(100, 125, 400, 125)
 
             can.line(100, 500, 100, 125)
             can.line(200, 500, 200, 125)
             can.line(300, 500, 300, 125)
             can.line(400, 500, 400, 125)
-            can.line(500, 500, 500, 125)
-            can.line(600, 500, 600, 125)
-            can.line(700, 500, 700, 125)
+            can.line(500, 500, 500, 200)
+            can.line(600, 500, 600, 200)
+            can.line(700, 500, 700, 200)
+
+            # Add the days (good lord there must be a better way)
+            can.setFontSize(13)
+            for i in range(6):
+                can.drawString(105+(i*100), 485, str(i+1))
+            for i in range(6):
+                can.drawString(105+(i*100), 410, str(i+7))
+            for i in range(6):
+                can.drawString(105+(i*100), 335, str(i+13))
+            for i in range(6):
+                can.drawString(105+(i*100), 260, str(i+20))
+            for i in range(3):
+                can.drawString(105+(i*100), 185, str(i+27))
 
 
             # Skip to next page
